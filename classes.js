3368fb0f91648efd8a276c30f9e0aa1bc2491511
Alina Kireeva <allina.kireeva88@gmail.com>
2018-10-04 23:22:14 +1300

    changed classes and style files

--- classes.js
+++ classes.js
@@ -3,19 +3,31 @@
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
-function start () {
-  // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
-  // Your turn! Create a new function called `two`, then call it from here.
+function start() {
+    // The first example is done for you. Uncomment the line below and reload the browser.
+    one()
+    two()
+    three()
+
+    // Your turn! Create a new function called `two`, then call it from here.
 }
 
-function one () {
-  // First, we have to find the element:
-  var one = document.getElementById('one')
+function one() {
+    // First, we have to find the element:
+    var one = document.getElementById('one')
 
-  // Next, we apply a new CSS class to it:
-  one.classList.add('blue')
+    // Next, we apply a new CSS class to it:
+    one.classList.add('blue')
+}
+
+function two() {
+    var two = document.getElementById('two')
+    two.classList.add('green')
+}
+
+function three() {
+    var three = document.getElementById('three')
+    three.classList.add('pink')
 }
 
 // CREATE FUNCTION two HERE
